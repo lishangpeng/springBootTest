481255e9fbb3e1402621ba29f5559e72641c2f2d
nibuxiangta1. <1214945875@qq.com>
2018-10-17 10:41:36 +0800

    测试了springBoot访问jsp页面

--- src/main/java/com/how2java/springboot/web/HelloController.java
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -1,13 +1,17 @@
 package com.how2java.springboot.web;
+import java.text.DateFormat;
+import java.util.Date;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
- 
-@RestController//@ResponseBody和@Controller的缩写
+  
+@Controller
 public class HelloController {
- 
+  
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello Spring Boot!";
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
     }
- 
 }
