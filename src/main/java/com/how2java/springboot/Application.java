ffa4cd642821dde45342f42ef0ac1d187e101fb8
nibuxiangta1. <1214945875@qq.com>
2018-10-17 09:51:39 +0800

    使用第一感觉和ssm相比简便了太多太多了，配置变少了，操作也简便了

---
+++ src/main/java/com/how2java/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.how2java.springboot;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication//表示是一个springBoot项目
+public class Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+ 
+}
