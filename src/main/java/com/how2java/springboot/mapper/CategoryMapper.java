d5c4d30c3a5ffe3a8fee98b940fed35819fbac9f
nibuxiangta1. <1214945875@qq.com>
2018-10-17 22:35:34 +0800

    测试了文件上传

---
+++ src/main/java/com/how2java/springboot/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.how2java.springboot.mapper;
+ 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.how2java.springboot.pojo.Category;
+ 
+@Mapper
+public interface CategoryMapper {
+    List<Category> findAll();
+}
